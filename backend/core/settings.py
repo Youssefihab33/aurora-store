ec3bd43b06411930789178fe9e13bdabc99d7181
Youssef Ihab <youssefihab33@gmail.com>
2025-02-20 02:30:47 +0200

    fully working model for adds

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.parent / 'data'
 
 # Default primary key field type
